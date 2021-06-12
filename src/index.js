9d814868262a89d5dfd82f32d9c862b3d98670e8
Wesley <wesleybortolosocco@gmail.com>
2021-06-11 21:42:51 -0300

    final version

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const pdf = require("html-pdf");
+const ejs = require('ejs');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+
+  ejs.renderFile('./pages/index.ejs', { name: 'Wesley', cidade: 'São Paulo', curso: 'Cièncias da Computação', horas: '20 horas', data:'10/06/2021' }, (err, html) => {
+    if (err) {
+      return res.status(500).json({message: 'Error in server '});
+    }
+
+    const options = {
+      format: 'A4',
+      border: {
+        right: '8'
+      }
+    };
+
+    pdf.create(html, options).toFile('./uploads/report.pdf', (error, response) => {
+      if (!error) {
+          return res.json({ message: 'PDF Generated, /download to download PDF' });
+       } else {
+          return res.json({ message: 'Fail in Generated PDF'});
+        }
+      })
+  });
+});
+
+  app.get('/download', (req, res) => {
+
+    res.type('pdf');
+    res.download('./uploads/report.pdf')
+  });
+
+app.listen(3333);
